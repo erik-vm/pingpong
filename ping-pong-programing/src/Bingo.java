8886730777c3c864ec35da02bb773db513407792
Erik <erik.vainumae@gmail.com>
2022-11-19 16:42:13 +0200

    finished bingo kata

--- ping-pong-programing/src/Bingo.java
+++ ping-pong-programing/src/Bingo.java
@@ -13,31 +13,33 @@ public class Bingo {
     List<Integer> nColumn = new ArrayList<>();
     List<Integer> gColumn = new ArrayList<>();
     List<Integer> oColumn = new ArrayList<>();
+    List<Integer> pickedNumbers = new ArrayList<>();
+
 
     public void playBingo(List<Integer> bingoCard) {
-        int[] availableNumber = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-                16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-                31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45,
-                46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
-                61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75};
-
-        List<Integer> pickedNumbers = new ArrayList<>();
-
-            int nextNr = 0;
-
-            while (!bingoCard.isEmpty()){
-                nextNr = availableNumber[random.nextInt(availableNumber.length)];
-                if (!pickedNumbers.contains(nextNr)){
-                    pickedNumbers.add(nextNr);
-                }
-                if (bingoCard.contains(nextNr)){
-                    bingoCard.remove(nextNr);
-                }
-
+        int numberCounter = 0;
+        int nextNr = randomNumber();
+        while (!bingoCard.isEmpty()) {
+            while (pickedNumbers.contains(nextNr)) {
+                nextNr = randomNumber();
             }
-        System.out.println("BINGO!");
+            pickedNumbers.add(nextNr);
+            if (bingoCard.contains(nextNr)) {
+                bingoCard.remove((Integer) nextNr);
+            }
+            numberCounter++;
+        }
+        System.out.println("==========================");
+        System.out.println("BINGO!!! BINGO!!! BINGO!!!");
+        System.out.println("==========================");
+
+        System.out.println("Total numbers drawn: " + numberCounter);
+        System.out.println("Drawn numbers: " + pickedNumbers);
     }
 
+    private int randomNumber(){
+        return random.nextInt(1,75);
+    }
 
     public List<Integer> generateBingoCard() {
         columnB();
